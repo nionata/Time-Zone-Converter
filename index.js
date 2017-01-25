52bc798ca10ce0e1600099d11b8d76e469986cca
nionata <n.ionata129@gmail.com>
2017-01-24 20:43:37 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,41 @@
+function myTime(array) {
+    //New array with converted times
+    var convertedTimes = [];
+
+    //Timezone difference between current location based
+    var rawTimeData = new Date().getTimezoneOffset();
+    var currentZone = rawTimeData / -60;
+
+    //Convert from each members time to users timezone
+    array.members.forEach(function(element) {
+        var memZone = parseInt(element["timezone"]);
+        var conversionFactor = currentZone - memZone;
+        convertedTimes.push(parseInt(element["availTime"]) + conversionFactor);
+    });
+
+    console.log(convertedTimes);
+}
+
+var team =
+        {
+            "name": "Meerkat Sundial",
+            "members": [
+                {
+                    "name": "@musecode",
+                    "availTime": "8",
+                    "timezone": "+1"
+                },
+                {
+                    "name": "@nionata",
+                    "availTime": "5",
+                    "timezone": "-5"
+                },
+                 {
+                    "name": "@ejour",
+                    "availTime": "10",
+                    "timezone": "+4"
+                }
+            ]
+        }
+
+myTime(team);
